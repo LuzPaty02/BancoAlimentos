e90e937cee57237b41e2c56b64f4f3016256ca84
Edwin Iniguez Moncada <110923425+EdwinIniguez@users.noreply.github.com>
2024-11-09 15:24:44 -0600

    Update Main.tsx

---
+++ views/Main.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { getAuth, signOut } from 'firebase/auth';
+// import Maps from './Map';
+import AddNecesidad from './AddNecesidad';
+
+export default function MainMap() {
+  const auth = getAuth();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('User signed out');
+      })
+      .catch((error) => {
+        console.error('Error signing out:', error);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* <Text style={styles.header}>Mapa de Donantes Activos</Text> */}
+      {/* <Maps /> */}
+      <AddNecesidad />
+      <View style={styles.button}>
+        <Button title="Sign Out" onPress={handleSignOut} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Ensures the container takes up the full screen
+    padding: 16,
+    justifyContent: 'space-between', // Spreads out items
+    backgroundColor: '#ffffff',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  button: {
+    marginBottom: 20, // Adds some margin to the bottom
+    justifyContent: 'flex-end',
+  },
+});
